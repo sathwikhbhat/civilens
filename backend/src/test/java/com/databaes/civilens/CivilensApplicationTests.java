661ad10d300d9760fdde30ac3387e1b66cadcc8a
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2026-03-01 06:17:54 +0530

    chore: initial project setup with Maven and base application structure

---
+++ backend/src/test/java/com/databaes/civilens/CivilensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.databaes.civilens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivilensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
